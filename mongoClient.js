8f728f478fce1294dd6764fbc39c1f40b336f3be
Mehrab Hossain <dev.mehrabhossain@gmail.com>
2025-08-01 20:35:41 +0600

    added mongoDB

--- mongoClient.js
+++ mongoClient.js
@@ -1,10 +1,18 @@
 import { MongoClient } from "mongodb";
 
-const url = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
+import dotenv from "dotenv";
+dotenv.config();
 
+const url = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
 const dbName = "usersDB";
 
-const client = new MongoClient(url);
+// Pass extra options only if using Atlas (mongodb+srv)
+const client = new MongoClient(
+  url,
+  url.startsWith("mongodb+srv")
+    ? { useNewUrlParser: true, useUnifiedTopology: true }
+    : {}
+);
 export let db;
 
 export async function connectDB() {
