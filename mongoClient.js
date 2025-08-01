797f69faafdbba0a0b112c6c8c963ceb25625946
Mehrab Hossain <dev.mehrabhossain@gmail.com>
2025-08-01 20:18:34 +0600

    added mongoDB

--- mongoClient.js
+++ mongoClient.js
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 
-const url = "mongodb://127.0.0.1:27017";
+const url = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
+
 const dbName = "usersDB";
 
 const client = new MongoClient(url);
